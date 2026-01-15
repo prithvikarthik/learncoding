f1be403b978d34138df7af2a9343526283627233
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-15 16:28:10 +0530

    chapter 2 exercise 4 of C programming

---
+++ C/ModernC/chap2_ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y, z;
+	float p, q, r;
+
+	printf("x = %d, y = %d, z = %d\n", x, y, z);
+	printf("p = %f, q = %f, r = %f\n", p, q, r);
+
+	return 0;/*values are changing after each time I ran it*/
+}	
