8197e219ceb7cbbba39cbb503bea7a0ab2d25d8f
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-17 10:14:10 +0530

    chapter 3 exercise 1

---
+++ C/ModernC/chap3_ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("(a) %6d, %4d\n", 86, 1040);
+        printf("(b) %12.5e\n", 30.253);
+	printf("(c) %.4f\n", 83.162);		
+	printf("(d) %-6.2g\n", .0000009979);
+
+	return 0;	
+}	
