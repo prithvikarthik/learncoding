cbac36f2c65c42ed68886ebd5533bbe31af3672c
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-17 19:50:06 +0530

    chapter 3 exercise 2

--- C/ModernC/chap3_ex2.c
+++ C/ModernC/chap3_ex2.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-	float x = 1.00;/*we need to change the value of x in the program itself(this is according to the question)*/
-	printf("(a) %-8.1e\n", x);
-	printf("(b) %10.6e\n", x);
-	printf("(c) %-8.3f\n", x);
-	printf("(d) %6f\n", x);
+	float x = 1.00;/*the value of x can only be changed inside the program itself.*/
+
+	printf("(a)%-8.1e\n", x);
+	printf("(b)%10.6e\n", x);
+	printf("(c)%-8.3f\n", x);
+	printf("(d)%6f\n", x);
 
 	return 0;
-
-}
+}	
