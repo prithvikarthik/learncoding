67ce11c3f4a3746a3210856475faf073e9898fa8
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-20 19:28:13 +0530

    chapter 4 exercise 3

---
+++ C/ModernC/chap4_ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("(a) %d\n", 8 / 5);
+	printf("(b) %d\n", -8 / 5);
+	printf("(c) %d\n", 8 / -5);
+	printf("(d) %d\n", -8 / -5);//this program was supposed to be done in C89, to see if any of these values get more than 1 value.
+				
+	return 0;
+}	
