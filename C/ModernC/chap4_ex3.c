dd49f9de650934bd02d1c04064ae95b945fac0c1
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-20 19:56:45 +0530

    chapter 4 exercise 3

--- C/ModernC/chap4_ex3.c
+++ C/ModernC/chap4_ex3.c
@@ -5,7 +5,7 @@ int main(void)
 	printf("(a) %d\n", 8 / 5);
 	printf("(b) %d\n", -8 / 5);
 	printf("(c) %d\n", 8 / -5);
-	printf("(d) %d\n", -8 / -5);//this program was supposed to be done in C89, to see if any of these values get more than 1 value.
-				
+	printf("(d) %d\n", -8 / -5);//There are no values more than 1 and the output is not changing(experimented on C89).
+
 	return 0;
 }	
