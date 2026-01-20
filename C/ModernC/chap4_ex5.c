6a67c4799c9cd6eb09648e9b3739951a533c8b5f
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-20 20:11:20 +0530

    chapter 4 exercise 5

---
+++ C/ModernC/chap4_ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("(a) %d\n", 8 % 5);
+	printf("(b) %d\n", -8 % 5);
+	printf("(c) %d\n", 8 % -5);
+	printf("(d) %d\n", -8 % -5);/*Not more than 1 value and has given the same output after every run(experimented on C89)*/
+
+	return 0;
+}	
