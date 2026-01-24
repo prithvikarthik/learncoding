6132d11b95787eb459970119ca29a48787d57eeb
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-24 22:08:10 +0530

    chapter 4 project1

---
+++ C/ModernC/chap4_pp1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	printf("Enter a two-digit number: ");
+	scanf("%d", &a);
+
+	printf("The reversal is: ");
+	printf("%d\n", ((a % 10) * 10) + a / 10);
+
+	return 0;
+}	
