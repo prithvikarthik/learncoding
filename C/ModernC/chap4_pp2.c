424b124d3e9d29d4e84b189635c8b365b8b7a878
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-24 22:27:02 +0530

    chapter 4 project2

---
+++ C/ModernC/chap4_pp2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	printf("Enter a three-digit number: ");
+	scanf("%d", &a);
+
+	printf("The reversal is: ");
+	printf("%d\n", ((a % 100) % 10) * 100 + ((a % 100) - (a % 100) % 10) + (a / 100));
+
+	return 0;
+}	
