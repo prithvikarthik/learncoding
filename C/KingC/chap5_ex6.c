26ea76a296608bf380dfcd487916ee63343d8058
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-30 19:57:19 +0530

    chapter 5 exercise 6

---
+++ C/KingC/chap5_ex6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("Enter a integer: ");
+	scanf("%d", &n);
+
+	if (n == 1 - 10)
+	{
+	   printf("n is between 1 and 10\n");
+	}	
+
+	return 0;
+}	
