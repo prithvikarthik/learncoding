3c3b7d1a501b13a2a8121ee27485c496730eaf04
Prithvi Karthik <k.prithvi@outlook.com>
2026-01-30 17:53:24 +0530

    chapter 5 exercise 5

---
+++ C/KingC/chap5_ex5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("Enter an integer: ");
+	scanf("%d", &n);
+
+	if (n >= 1 <= 10);
+	{
+	   printf("n is between 1 and 10\n");/*this 'if' statement is not legal(not right)*/
+	}
+
+	return 0;
+}	
