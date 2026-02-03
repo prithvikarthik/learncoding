9728a2ebbdcbbdbe4558ac09edb377365fc38bbb
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-03 06:44:36 +0530

    chapter 5 exercise 10

---
+++ C/KingC/chap5_ex10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1;
+
+	switch (i % 3) {
+	  case 0: printf("zero\n");
+		  break;
+	  case 1: printf("one\n");
+		  break;
+	  case 2: printf("two\n");
+		  break;
+	}
+
+	return 0;
+}	
