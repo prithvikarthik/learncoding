3d53b87662530e579d1acafdfc4fd9b3ee8a8c27
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-04 06:32:11 +0530

    chapter 5 project 6

---
+++ C/KingC/chap5_pp6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, checksum_digit;
+
+	printf("Enter the first 11 digits: ");
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &a1, &a2, &a3, &a4, &a5, &a6, &a7, &a8, &a9, &a10, &a11, &checksum_digit);
+
+	int first_sum = a1 + a3 + a5 + a7 + a9 + a11;
+	int second_sum = a2 + a4 + a6 + a8 + a10;
+	int total = 3 * first_sum + second_sum;
+
+	if (checksum_digit != 9 - ((total - 1) % 10)){
+	   printf("NOT VALID, try again\n");
+	}
+	else {
+	   printf("VALID, checksum digit is %1d\n", checksum_digit);
+	}
+		
+	
+	return 0;
+}
