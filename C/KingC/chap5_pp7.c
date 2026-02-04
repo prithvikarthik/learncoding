5fca355dcf3276c11c50825ad1f658209a58bbc5
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-04 19:46:57 +0530

    chapter 5 project 7

---
+++ C/KingC/chap5_pp7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2, num3, num4;
+
+	printf("Enter 4 integers: ");
+	scanf("%d, %d, %d, %d", &num1, &num2, &num3, &num4);
+
+	if (){
+	   printf("%d\n", );
+	}
+	if (){
+	   printf("%d\n", );
+	}   
+	if (){
+	   printf("%d\n", );
+	}   
+	if (){
+	   printf("%d\n", );
+	}   
+
+	return 0;
+}	
