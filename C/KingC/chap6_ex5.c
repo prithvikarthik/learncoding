d54e7490e9a5e4b6fbbf1ace5506ef5d4a396b9e
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-09 18:40:24 +0530

    chapter 6 exercise 5

--- C/KingC/chap6_ex5.c
+++ C/KingC/chap6_ex5.c
@@ -2,21 +2,21 @@
 
 int main(void)
 {
-	printf("(a) ");
-	for (int i1 = 0; i1 < 10; i1++){
+	printf("(a)");
+	while (int i1 < 10){
 	  printf("%d ", i1);
 	}
 	printf("\n");
 
-	printf("(b) ");
-	for (int i2 = 0; i2 < 10; ++i2){
-	  printf("%d ", i2);
+	printf("(a)");
+	while (int i1 < 10){
+	  printf("%d ", i1);
 	}
 	printf("\n");
-
-	printf("(c) ");
-	for (int i3 = 0; i3++ < 10; ){
-	  printf("%d ", i3);
+	
+	printf("(a)");
+	while (int i1 < 10){
+	  printf("%d ", i1);
 	}
 	printf("\n");
-}
+}	
