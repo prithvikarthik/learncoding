43aa4cb89c2cfbb784b983bfef6c936ab4560a39
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-09 17:53:25 +0530

    chapter 5 project

--- C/KingC/chap5_pp7.c
+++ C/KingC/chap5_pp7.c
@@ -4,20 +4,39 @@ int main(void)
 {
 	int num1, num2, num3, num4;
 
-	printf("Enter 4 integers: ");
+	printf("Enter 4 integers(make sure all of them are different): ");
 	scanf("%d, %d, %d, %d", &num1, &num2, &num3, &num4);
 
-	if (){
-	   printf("%d\n", );
+	int set1max, set1min, set2max, set2min;
+
+
+	if (num1 > num2){
+	     set1max = num1;
+	     set1min = num2;
+	}     
+        else{
+	     set1max = num2;
+	     set1min = num1;
 	}
-	if (){
-	   printf("%d\n", );
+	if (num3 > num4){
+	     set2max = num3;
+	     set2min = num4;
+	}     
+	else{
+	     set2max = num4;
+	     set2min = num3;
+	}     
+	if (set1max > set2max){
+	   printf("Largest: %d\n", set1max);
 	}   
-	if (){
-	   printf("%d\n", );
+	else{  
+	   printf("Largest: %d\n", set2max);
 	}   
-	if (){
-	   printf("%d\n", );
+	if (set1min < set2min){
+	   printf("Smallest: %d\n", set1min);
+	}   
+	else{  
+	   printf("Smallest: %d\n", set2min);
 	}   
 
 	return 0;
