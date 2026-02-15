9e61d2434a54bb1aaf201022a55a1c5a7fef70ec
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-15 14:17:06 +0530

    chapter 6 exercise 12

--- C/KingC/chap6_ex12.c
+++ C/KingC/chap6_ex12.c
@@ -10,16 +10,17 @@ int main(void)
 	  if (n == 1) {
 	    printf("Your number is neither composite nor prime.\n");
 	  }  
-
-	for (int d = 2; (d * d) < n; d++) {
+	
+	  int d = 1;
+	  if (n % d != 0 && n % d > 0) {
+	    printf("Your number is prime\n");
+	    break;
+	for (; (d * d) < n; d++) {
 	  if (n % d == 0) {
 	    printf("Your number is composite(not prime).\n");	  
 	    break;
 	  }
 
-	  else {
-	    printf("Your number is prime\n");
-	    break;
 	  }  
 
 	}
