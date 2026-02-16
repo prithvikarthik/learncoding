e90940e43fc09a7a7b84c162bcf0e1b0a56bc299
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-16 16:42:34 +0530

    chapter 6 exercise 12

--- C/KingC/chap6_ex12.c
+++ C/KingC/chap6_ex12.c
@@ -2,28 +2,24 @@
 
 int main(void)
 {
-	int n;
+	int n, d; 
 
 	printf("Enter a positive integer: ");
 	scanf("%d", &n);
 
-	  if (n == 1) {
-	    printf("Your number is neither composite nor prime.\n");
-	  }  
-	
-	  int d = 1;
-	  if (n % d != 0 && n % d > 0) {
-	    printf("Your number is prime\n");
-	    break;
-	for (; (d * d) < n; d++) {
-	  if (n % d == 0) {
-	    printf("Your number is composite(not prime).\n");	  
-	    break;
-	  }
-
-	  }  
-
+	if (n == 1) {
+	  printf("Your number is neither composite nor prime.\n");
+	  return 0;
 	}
+
+	for (d = 2; (d * d) <= n; d++) {
+	  if (n % d == 0) {
+	    printf("Your number is composite(not prime)\n");
+	    return 0;
+	  }
+	}  
+
+	  printf("Your number is prime\n");
 
 	return 0;
 
