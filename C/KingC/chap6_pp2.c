f53258c1b588549bef384d28dc07a513c4addc5f
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-16 19:41:13 +0530

    chapter 6 project 2

---
+++ C/KingC/chap6_pp2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int m, n;
+
+	printf("Enter two integers: ");
+	scanf("%d, %d", m, n);
+
+	if (n == 0 || m ==0) {
+	  printf("GCD of the numbers cannot be found.");
+	}
+
+	while ()
+}	
