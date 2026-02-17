f64efb2be16fcb588a3e2d6246f7408c3fe6c28a
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-17 14:32:44 +0530

    chapter 6 project 2

--- C/KingC/chap6_pp2.c
+++ C/KingC/chap6_pp2.c
@@ -1,15 +1,24 @@
+/*This is Euclid's algorithm*/
 #include <stdio.h>
 
 int main(void)
 {
-	int m, n;
+	int m, n, GCD;
 
 	printf("Enter two integers: ");
-	scanf("%d, %d", m, n);
+	scanf("%d, %d", &m, &n);
 
-	if (n == 0 || m ==0) {
+	if (n == 0 || m == 0) {
 	  printf("GCD of the numbers cannot be found.");
+
+	}
+	while (n != 0) {
+	  GCD = m % n;
+	  m = n;
+	  n = GCD;
 	}
 
-	while ()
+	printf("Greatest Common Divisor(GCD): %d\n", m);
+
+	return 0;
 }	
