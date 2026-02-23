2483e7cdbdf5da80665580f24117638dc8b27e23
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-23 07:13:35 +0530

    chapter 6 project 6

--- C/KingC/chap6_pp6.c
+++ C/KingC/chap6_pp6.c
@@ -2,5 +2,23 @@
 
 int main(void)
 {
+	int n;
+
+	printf("Enter an integer value for n: ");
+	scanf("%d", &n);
+
+	printf("All the even squares between 1 and n are, \n");
 	
+	for (int i = 2; i * i <= n; i++) {
+	  if (i % 2 == 0) {
+	    printf("%d\n", i * i);
+	  }
+
+	  else if (i % 2 != 0) {
+	    continue;
+	  }  
+	}
+	printf("\n");
+
+	return 0;
 }
