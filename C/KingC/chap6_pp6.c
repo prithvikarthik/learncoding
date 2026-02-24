01f36bca69fef8cc64d98ce46293d4b39ed7f930
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-24 16:23:44 +0530

    chapter 6 project 6

--- C/KingC/chap6_pp6.c
+++ C/KingC/chap6_pp6.c
@@ -3,17 +3,19 @@
 int main(void)
 {
 	int n;
+	//int count = 0;
 
 	printf("Enter an integer value for n: ");
 	scanf("%d", &n);
 
 	printf("All the even squares between 1 and n are, \n");
 	
-	for (int i = 2; i * i <= n; i++) {
-	  if (i % 2 == 0) {
+	for (int i = 2; i * i <= n; i += 2) {	
 	    printf("%d\n", i * i);
-	  }  
+
+	  //++count;
 	}
+	//printf("I've taken %d iterations to print all the no.s\n", count);
 	printf("\n");
 
 	return 0;
