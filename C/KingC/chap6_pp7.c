190191145eb611e967a9b8768b958f3c284b04c5
Prithvi Karthik <k.prithvi@outlook.com>
2026-02-24 16:24:06 +0530

    chapter 6 project 7

--- C/KingC/chap6_pp7.c
+++ C/KingC/chap6_pp7.c
@@ -1,6 +1,23 @@
+//Prints a table of squares using a method without using multiplication.
 #include <stdio.h>
 
 int main(void)
 {
+	int n, odd, square;
+
+	printf("This program prints a table of squares.\n\n");
+	printf("Enter the number of entries in the table: ");
+	scanf("%d", &n);
 	
+	square = 1;
+	odd = 3;
+	for (int i = 1; i <= n; i++) {
+	  printf("%10d%10d\n", i, square);
+
+	  square += odd;
+	  odd += 2;
+	}
+
+	return 0;
+
 }
