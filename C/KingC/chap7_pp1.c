b60ed754c1fb32691091d96a21e60239abcab355
Prithvi Karthik <k.prithvi@outlook.com>
2026-03-09 17:02:18 +0530

    chapter 7 project 1

---
+++ C/KingC/chap7_pp1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int i , n;
+    
+   printf("This program prints a table of squares.\n");
+   printf("Enter number of entries in table: ");
+   scanf("%d", &n);
+    
+   for (i = 1; i <= n; i++)
+   {
+     printf("%20d%20d\n", i , i * i);
+   }
+
+   return 0;
+
+}
