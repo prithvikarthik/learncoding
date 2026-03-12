fdb870b7deea0c2c9566841173785d95444e7a37
Prithvi Karthik <k.prithvi@outlook.com>
2026-03-12 17:42:41 +0530

    chapter 7 project 6

---
+++ C/KingC/chap7_pp6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("The size of an int is %ld\n\n", sizeof(int));
+    printf("The size of a short is %ld\n\n", sizeof(short));
+    printf("The size of a long is %ld\n\n", sizeof(long));
+    printf("The size of a float is %ld\n\n", sizeof(float));
+    printf("The size of an double is %ld\n\n", sizeof(double));
+    printf("The size of an long double is %ld\n\n", sizeof(long double));
+
+    return 0;
+}    
