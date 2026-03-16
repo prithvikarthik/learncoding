9ea38a93bdeb7d3e8b5b60b82b63f1411af2fd02
Prithvi Karthik <k.prithvi@outlook.com>
2026-03-16 16:45:39 +0530

    chapter 8 project 1

---
+++ C/KingC/chap8_pp1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void)
+{
+    bool digit_seen[10] = {false};
+    bool repeated_digit[10] = {false};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while (n > 0)
+    {
+      digit = n % 10;  
+      if (digit_seen[digit] == true)
+      {
+        repeated_digit[digit] = true;
+        n /= 10;
+        continue;
+      }
+      digit_seen[digit] = true;
+      n /= 10;
+    }
+
+    printf("Repeated digit(s): ");
+
+    for (int i = 0; i < 10; i++)
+    {
+      if (repeated_digit[i] == true)
+      {
+        printf("%d ", i);
+      }
+    }
+    printf("\n");
+
+    return 0;
+
+} 
+
+
+
+
+
+
+
+
+
+
+
