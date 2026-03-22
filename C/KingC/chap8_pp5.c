12d7a608ed0e7812fb3a970664e7080d13b56424
Prithvi Karthik <k.prithvi@outlook.com>
2026-03-22 16:28:43 +0530

    chapter 8 project 5

--- C/KingC/chap8_pp5.c
+++ C/KingC/chap8_pp5.c
@@ -5,5 +5,45 @@
 
 int main(void)
 {
+    int low_rate, num_years, months;
+    double value[5];
+
+    printf("Enter interest rate: ");
+    scanf("%d", &low_rate);
+
+    printf("Enter number of years: ");
+    scanf("%d", &num_years);
+
+    printf("\nYears");
+
+    for (int i = 0; i < NUM_RATES; i++)
+    {
+      printf("%9d%%", low_rate + i);
+      value[i] = INITIAL_BALANCE;
+    }
+    printf("\n");
+
+    for(months = 1; months <= (num_years * 12); months++)
+    {
+      if (months % 12 == 0)
+      {
+        printf("%3d    ", months / 12);
+      }
+
+      for (int i = 0; i < NUM_RATES; i++)
+      {
+        value[i] += (low_rate + i) / 100.00 * value[i];
+        if (months % 12 == 0)
+        {
+          printf("%10.2f", value[i]);
+        }
+      }
+      if (months % 12 == 0)
+      {
+        printf("\n");
+      }
+    }
+
+    return 0;
 
 }
