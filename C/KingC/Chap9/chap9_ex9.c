a2e70a641e62374d590fbfb719350d0ad38d06b2
Prithvi Karthik <k.prithvi@outlook.com>
2026-03-28 19:58:15 +0530

    chapter 9 exercise 9(need to remember, very important)

---
+++ C/KingC/Chap9/chap9_ex9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(void)
+{
+    int i = 1;
+    int j = 2;
+    
+    printf("Now,\ni = %d and j = %d\n\n", i, j);
+
+    swap(i, j);
+
+    printf("If we swap them,\n");
+    printf("i = %d, j = %d\n", i, j);
+
+    return 0;
+
+}
